926c6fd41eb778699f0384858f8d5fba5362bed0
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-28 17:53:36 -0400

    Completed a few array exercises

---
+++ 102.rb
@@ -0,0 +1,39 @@
+# --- Question 1 ---------
+# Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+# array = ["fruit", "dessert", "main course"]
+# array.push("jello", "yellow")
+# pp array 
+
+# --- Question 2 -------
+# Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+# array = ["A", "B", "C", "D"]
+# array[1] = 4
+# pp array
+
+# ----- Question 3 -------
+# Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+# index = 0
+# numbers = [3, 1, 29, 40, 12]
+# while index < numbers.length
+#   puts numbers[index]
+#   index += 1
+# end
+
+#----Question 4---------
+#Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+# array = [20]
+# array.push(4, 5, 30)
+# pp array 
+
+#------Question 5------
+# Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+# array = ["bunny", "cat", "dog"]
+# pp array[2].upcase
+
+
+
+
