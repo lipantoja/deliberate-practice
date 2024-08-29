de6b38c5504a1c9cfe5d0d286d00fc402e71fcc9
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-08-29 17:50:52 -0400

    printed names on seperate lines

--- 102.rb
+++ 102.rb
@@ -34,6 +34,15 @@
 # array = ["bunny", "cat", "dog"]
 # pp array[2].upcase
 
+#-------- Question 6 ----------
+# Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+array = ["Alexander", "John", "Jessie"]
+index = 0
+while index < array.length
+  puts array[index]
+  index += 1
+end
 
 
 
