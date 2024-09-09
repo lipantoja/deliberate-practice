e0c158af6b710a7ff1a11d384bfb886d9d93065c
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-09-09 16:12:44 -0400

    pratice pulling from hash

---
+++ 103.rb
@@ -0,0 +1,31 @@
+#----question 1-----
+# Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+# person = {name: "Alex", last_name: "Hamilton", email: "aham@gmail.com"}
+
+# p person[:name]
+# p person[:last_name]
+# p person[:email]
+
+# puts person [:name], [:last_name], [:email]
+
+#---question 2---
+# Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+# persons = [
+#   {first_name: "Jon", last_name: "Johnson" },
+#   {first_name: "Laura", last_name: "Smith"},
+#   {first_name: "Frank", last_name: "Ocean"}
+# ]
+# index = 0
+# while index < 3
+#   # want it to pick first hash [0] then pick first item in array [0]
+#   name = persons [index][:first_name]
+#   index += 1
+#   pp name
+# end
+
+
+
+
+
