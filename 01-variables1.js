3e88eb9e371e058ec38c5d17b4400bb2ec04af5b
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-09-15 17:19:02 -0400

    Variables javascript practice

---
+++ 01-variables1.js
@@ -0,0 +1,40 @@
+// 01-Variables1: Question 1
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+// var x = 10
+// var y = 10
+// z = x + y
+
+// var z = 10 + 10
+// console.log (z);
+
+// var number = 5
+// console.log (number + 10);
+
+// 01-Variables1: Question 2
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
+// var word = 'hello'
+// console.log(word.toUpperCase());
+
+// 01-Variables1: Question 3
+// Write a program that uses variables to store two numbers, then prints the numbers added together.
+// var x = 10;
+// var y = 20;
+// console.log(x + y);
+
+// 01-Variables1: Question 4
+// Write a program that uses a variable to store a word, then prints that word in reverse order.
+// var word = "elevator"
+// console.log(word.split('').reverse('').join(''));
+
+// 01-Variables1: Question 5
+// Write a program that uses a variable to store a number, then prints the number times 10.
+// let number = 4;
+// console.log(number * 10);
+
+// 01-Variables1: Question 6
+// Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+// var x = "Hello"
+// var y = "World"
+// var phrase = x + ' ' + y
+// console.log (phrase.toUpperCase());
+
